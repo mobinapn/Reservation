fe220ed5b892ac00b04a0bc784d41b4637e9c2e7
si.eslami <si.eslami@mail.sbu.ac.ir>
2024-10-04 18:25:21 +0330

    initialize the project

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
