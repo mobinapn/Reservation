b1bf45cd5d60bfb47f0acc001bff913a5b2157c2
si.eslami <si.eslami@mail.sbu.ac.ir>
2024-10-05 20:29:55 +0330

    add staticfiles and update .gitignore

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reservation.db'
 db = SQLAlchemy(app)
 
 
