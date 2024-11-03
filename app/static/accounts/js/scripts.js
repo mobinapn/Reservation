f23754cd56d243ae20b9200fda09f5e555aeaced
mobinapn <m.paknia81@gmail.com>
2024-11-03 14:59:50 +0330

    admin pannel created

--- app/static/accounts/js/scripts.js
+++ app/static/accounts/js/scripts.js
@@ -18,3 +18,19 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
+
+
+const errorMessage = document.getElementById("error");
+const progressBar = document.getElementById("progress-bar");
+const duration = 5000; // مدت زمان نمایش پیام به میلی‌ثانیه (5 ثانیه)
+
+// شروع پر کردن نوار پیشرفت
+progressBar.style.width = "100%";
+
+// محو کردن پیام بعد از مدت زمان مشخص
+setTimeout(() => {
+    errorMessage.style.opacity = 0; // محو کردن پیام
+    setTimeout(() => {
+        errorMessage.style.display = 'none'; // پنهان کردن پیام
+    }, 300); // زمان محو شدن کامل
+}, duration);
